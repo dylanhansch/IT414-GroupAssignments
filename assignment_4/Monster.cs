c3dd3b322375a17c251fa1618e49b2516d09e9ce
Yohannes Habte <yohannes.habte@mnsu.edu>
2020-02-06 16:46:59 -0600

    generic update

--- assignment_4/Monster.cs
+++ assignment_4/Monster.cs
@@ -6,17 +6,17 @@ using System.Threading.Tasks;
 
 namespace Assignment4
 {
-    abstract class Monster:IAction
+    abstract class Monster<T>:IAction
     {
         public int Level = 1;
         public int Health = 10;
 
-        public string Attack()
+        public string Attack(T Player)
         {
             return "Attacking!";
         }
 
-        public string Defend()
+        public string Defend(T Player)
         {
             return "Defending.";
         }
